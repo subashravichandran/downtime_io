d2ac40a5ebc3f16d1a1582bfe917d8fb299a0e3e
subash <waytosubash@gmail.com>
2025-09-30 19:23:07 +0530

    Admin config - create and delte

--- engines/admin/app/controllers/admin/setups_controller.rb
+++ engines/admin/app/controllers/admin/setups_controller.rb
@@ -3,5 +3,30 @@ module Admin
     def index
       @setups = Setup.all
     end
+
+    def new
+      @setup = Setup.new
+    end
+
+    def create
+      @setup = Setup.new(setup_params)
+      if @setup.save
+        redirect_to setups_path, notice: "Config created successfully"
+      else
+        render :new
+      end
+    end
+
+    def destroy
+      @setup = Setup.find(params[:id])
+      @setup.destroy
+      redirect_to setups_path, notice: 'Config deleted'
+    end
+
+    private
+
+    def setup_params
+      params.require(:setup).permit(:site_name, :url)
+    end
   end
 end
--- engines/admin/config/routes.rb
+++ engines/admin/config/routes.rb
@@ -1,4 +1,4 @@
 Admin::Engine.routes.draw do
   root "setups#index"
-  resources :setups, only: :index
+  resources :setups, only: [:index, :new, :create, :destroy]
 end
