b5ab4284240ae0d17c24121c2500991e3e0df41d
subash <waytosubash@gmail.com>
2025-09-30 20:03:23 +0530

    Admin config - UI/UX, show as table

--- engines/admin/app/models/admin/setup.rb
+++ engines/admin/app/models/admin/setup.rb
@@ -1,4 +1,13 @@
 module Admin
   class Setup < ApplicationRecord
+    before_save :normalize_url
+
+    private
+
+    def normalize_url
+      return if url.blank?
+
+      self.url = url.start_with?('http') ? url : "https://#{url}"
+    end
   end
 end
